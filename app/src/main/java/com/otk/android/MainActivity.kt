21d37864fedc86ce175305453e505c4ea739833a
huangxy <hxy_coding@163.com>
2025-03-05 19:49:43 +0800

    init

--- app/src/main/java/com/otk/android/MainActivity.kt
+++ app/src/main/java/com/otk/android/MainActivity.kt
@@ -1,48 +1,66 @@
 package com.otk.android
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.otk.android.ui.theme.AndroidTheme
+import androidx.compose.ui.viewinterop.AndroidView
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactRootView
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+class MainActivity : ReactActivity() {
+
+    var DEV_BUNDLE_URL = "http://localhost:8081/index.bundle?platform=android"
+
+    override fun getMainComponentName(): String {
+        return "RNApp"
+    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        val builder = ReactInstanceManager.builder()
+//        builder.setApplication(application).setCurrentActivity(this)
+//            .setBundleAssetName("index.android.bundle").setJSMainModulePath("index")
+//            .addPackages(mutableListOf<ReactPackage>(MainReactPackage()))
+//            .setUseDeveloperSupport(reactNativeHost.useDeveloperSupport)
+//            .setInitialLifecycleState(LifecycleState.RESUMED)
+//            .setJsEngineAsHermes(true)
+//
+//        /**
+//         * dev
+//         */
+//        val loader: JSBundleLoader = object : JSBundleLoader() {
+//            override fun loadScript(delegate: JSBundleLoaderDelegate?): String {
+//                delegate?.setSourceURLs(
+//                    DEV_BUNDLE_URL, DEV_BUNDLE_URL
+//                );
+//                return DEV_BUNDLE_URL
+//            }
+//        }
+//        builder.setJSBundleLoader(loader)
+//
+//
+//        /**
+//         * view
+//         */
+//        val rootView = ReactRootView(this).apply {
+//            startReactApplication(builder.build(), mainComponentName)
+//        }
+//        setContentView(rootView)
+//    }
+}
+
+
+@Composable
+fun RNComposableView(register: ReactInstanceManager, module: String, bundle: Bundle?) {
+    AndroidView(modifier = Modifier.fillMaxSize(), factory = { context ->
+        //init
+        ReactRootView(context).apply {
+            startReactApplication(register, module, bundle)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello abcd $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidTheme {
-        Greeting("Android")
-    }
+    }, update = { view ->
+        println("view update")
+    })
 }
